348784b8d8f8714b17dede8a60c369360c19feb8
velioglu <velioglu@ceng.metu.edu.tr>
2017-12-26 14:43:32 +0300

    Remove broadcast join logic
    After this change all the logic related to shard data fetch logic
    will be removed. Planner won't plan any ShardFetchTask anymore.
    Shard fetch related steps in real time executor and task-tracker
    executor have been removed.

--- src/backend/distributed/citus--7.2-3--7.3-1.sql
+++ src/backend/distributed/citus--7.2-3--7.3-1.sql
@@ -1,4 +1,5 @@
 /* citus--7.2-3--7.3-1 */
 
-/* bump version to 7.3-1 */
-
+DROP FUNCTION IF EXISTS master_expire_table_cache(regclass);
+DROP FUNCTION IF EXISTS pg_catalog.worker_fetch_regular_table(text, bigint, text[], integer[]);
+DROP FUNCTION IF EXISTS pg_catalog.worker_fetch_foreign_file(text, bigint, text[], integer[]);
